39e9737080780e3339efaeed84f7dbbdf5b8ea36
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2024-04-23 12:40:57 +0200

    no env

--- config/config.php
+++ config/config.php
@@ -6,19 +6,19 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DB_HOST') ?: 'mysql';
-$CFG->dbname    = getenv('DB_NAME') ?: 'moodle';
-$CFG->dbuser    = getenv('DB_USERNAME') ?: 'moodle';
-$CFG->dbpass    = getenv('DB_PASSWORD') ?: 'moodle';
+$CFG->dbhost    = 'test-moodle-20240423091442367100000009.cjmyxronayb5.eu-west-1.rds.amazonaws.com';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'moodlezanichelli';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DB_PORT') ?: 3306,
+  'dbport' => 3306,
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_0900_ai_ci',
 );
 
-$CFG->wwwroot   = getenv('MOODLE_HOST') ?: 'http://localhost';
+$CFG->wwwroot   = 'http://test-moodle.aglebert.zanichelli.it';
 $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 
