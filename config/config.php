3051e85ceafaf2b1ee220a2fff91e7f1d10be0ff
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2024-04-23 13:01:05 +0200

    redis session

--- config/config.php
+++ config/config.php
@@ -24,6 +24,22 @@ $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
 
+@error_reporting(E_ALL | E_STRICT);   // NOT FOR PRODUCTION SERVERS!
+@ini_set('display_errors', '1');         // NOT FOR PRODUCTION SERVERS!
+$CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+$CFG->debugdisplay = 1;              // NOT FOR PRODUCTION SERVERS!
+
+$CFG->session_handler_class = '\core\session\redis';
+$CFG->session_redis_host = 'redis';
+$CFG->session_redis_port = 6379;  // Optional.
+$CFG->session_redis_database = 0;  // Optional, default is db 0.
+$CFG->session_redis_auth = ''; // Optional, default is don't set one.
+$CFG->session_redis_prefix = ''; // Optional, default is don't set one.
+$CFG->session_redis_acquire_lock_timeout = 120;
+$CFG->session_redis_acquire_lock_retry = 100; // Optional, default is 100ms (from 3.9)
+$CFG->session_redis_lock_expire = 7200;
+$CFG->session_redis_serializer_use_igbinary = false; // Optional, default is PHP builtin serializer.
+
 require_once(__DIR__ . '/lib/setup.php');
 
 // There is no php closing tag in this file,
