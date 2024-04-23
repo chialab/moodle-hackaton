fce5a3de6a888dd817681fcbc5199821c048cab4
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2024-04-23 13:02:39 +0200

    test-redis

--- config/config.php
+++ config/config.php
@@ -30,7 +30,7 @@ $CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION 
 $CFG->debugdisplay = 1;              // NOT FOR PRODUCTION SERVERS!
 
 $CFG->session_handler_class = '\core\session\redis';
-$CFG->session_redis_host = 'redis';
+$CFG->session_redis_host = 'test-redis';
 $CFG->session_redis_port = 6379;  // Optional.
 $CFG->session_redis_database = 0;  // Optional, default is db 0.
 $CFG->session_redis_auth = ''; // Optional, default is don't set one.
